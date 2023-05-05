27b57a1a03c570d55cdf9b3868d26f1342fffde1
chinmayraii <chinmayrai75@gmail.com>
2023-05-05 14:57:35 +0530

    'update'

--- app.py
+++ app.py
@@ -1,14 +1,78 @@
-from flask import Flask,render_templates
+from flask import Flask, render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///todo.db' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+db = SQLAlchemy(app)
 
-@app.route("/products")
-def products():
-    return "<p>products page</p>"
+
+
+
+class Todo(db.Model):
+    sno=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(200), nullable=False)
+    desc=db.Column(db.String(500), nullable=False)
+    date_created =db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+    
+
+
+
+@app.route("/", methods=['GET','POST'])
+def index():
+    if request.method=='POST':
+        title=request.form['title']
+        desc=request.form['desc']
+        todo=Todo(title=title,desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+        data=Todo.query.all()
+        return render_template('index.html',data=data)
+    else:
+        data=Todo.query.all()
+        return render_template('index.html',data=data)
+    
+
+
+@app.route("/delete/<int:sno>/")
+def delete(sno):
+    todo=Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
+
+@app.route("/update/<int:sno>/", methods=['GET','POST'])
+def update(sno):
+    if request.method=='POST':
+        title=request.form['title']
+        desc=request.form['desc']
+        todo=Todo.query.filter_by(sno=sno).first()
+        todo.title=title
+        todo.desc=desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect ('/')
+    else:
+        todo=Todo.query.filter_by(sno=sno).first()
+        return render_template('update.html', todo=todo)
+    
+@app.route("/search/", methods=['GET','POST'])
+def search():
+    if request.method=='POST':
+        find=request.form['name']
+        data=Todo.query.filter_by(title=find).all()
+        return render_template('index.html',data=data)
+    else:
+        data=Todo.query.all()
+        return render_template('index.html',data=data)
+
 
 if __name__ =='__main__':
     app.run(debug=True, port=8000)
